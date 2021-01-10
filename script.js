7d0ca76d391fc2df648553fa8d642381440d41e8
Andrea Pozza <andrea.pozza@hotmail.com>
2021-01-10 14:04:57 +0100

    feat: toggle expand on click

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var sidebarIcons = document.querySelectorAll("#side-bar-small .top .icon");
 var _loop_1 = function (icon) {
     icon.onclick = function () {
+        if (icon.classList.contains('selected'))
+            document.getElementById('side-expandable').classList.toggle('expanded');
+        else
+            document.getElementById('side-expandable').classList.add('expanded');
         sidebarIcons.forEach(function (el) { return el.classList.remove('selected'); }); //remove class 'selected' from every icons
         icon.classList.add('selected');
     };
--- script.ts
+++ script.ts
@@ -2,6 +2,8 @@ const sidebarIcons = document.querySelectorAll("#side-bar-small .top .icon")
 
 for (let icon of Array.from(sidebarIcons) as HTMLElement[]) {
     icon.onclick = () => {
+        if(icon.classList.contains('selected')) document.getElementById('side-expandable').classList.toggle('expanded')
+        else document.getElementById('side-expandable').classList.add('expanded')
         sidebarIcons.forEach(el=>el.classList.remove('selected')) //remove class 'selected' from every icons
         icon.classList.add('selected')
     }
